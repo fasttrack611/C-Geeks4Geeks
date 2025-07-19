b31d52bd05ff1b903553fcc9bb1a247108d92f5a
fasttrack611 <130031515+fasttrack611@users.noreply.github.com>
2025-07-19 06:09:30 +0530

    Update reverse_number.c

--- reverse_number.c
+++ reverse_number.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
 
-int main()
-{
-    int n, rev = 0;
-
-    printf("Enter Number to be reversed : ");
-    scanf("%d", &n);
-
-    int r = 0;
-      while (n != 0)
-    {
-        r = n % 10;
+// Function to reverse digits of an integer
+int reverseDigits(int n) {
+    int rev = 0;
+    while (n != 0) {
+        int r = n % 10;
         rev = rev * 10 + r;
         n /= 10;
     }
+    return rev;
+}
 
-    printf("Number After reversing digits is: %d", rev);
+int main() {
+    int n;
+    printf("Enter number to be reversed: ");
+    if (scanf("%d", &n) != 1) {
+        printf("Invalid input. Please enter an integer.\n");
+        return 1; // non-zero return for error
+    }
+
+    int reversed = reverseDigits(n);
+    printf("Number after reversing digits is: %d\n", reversed);
 
     return 0;
 }
